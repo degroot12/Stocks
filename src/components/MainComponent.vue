04e312f28f9cf1afd8be7382910cb65ad30b7e7a
Thomas de Groot <degroot12@gmail.com>
2021-11-19 18:01:22 +0100

    started project

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <Navbar />
+  <div>Welkom bij Thomas' aandelen app</div>
+</div>
+</template>
+
+<script>
+import Navbar from './Navbar/Navbar.vue'
+export default {
+    name: 'MainComponent',
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">Header</div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.container {
+    background-color: rgb(3, 3, 153);
+    padding: 1em;
+    color: white;
+}
+</style>
